f1bd3b54bb1e8163ed58d0fb76d368115d571bab
Christy Lum <christy.lum121@gmail.com>
2020-08-05 06:17:12 +0000

    Implemented initial logic to fetch cat breed data from the API endpoint using request

---
+++ breedFetcher.js
@@ -0,0 +1,26 @@
+const request = require("request");
+
+//Endpoint that will allow us to search breed information:
+//https://api.thecatapi.com/v1/breeds/search
+
+const fetchBreedDescription = function(breedName, callback) {
+  const breedName = process.argv.slice(2);
+
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, body) => {
+    if (error !== null) {
+      callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+
+      //if breed not found or left empty
+      if (data[0] === undefined || data[0] === []) {
+        callback("Oops! The breed you're looking for cannot be found", null);
+      } else {
+        let description = data[0].description;
+        callback(error, description);
+      }
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
